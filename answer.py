391bdde1df6c6c729c42a38dab3c1854687c442f
VaradSharmaCode <varad.sharma2009@gmail.com>
2021-08-15 11:36:32 +0530

    Added Wikipedia search
    Added a Wikipedia search functionality to the bot

--- answer.py
+++ answer.py
@@ -1,9 +1,16 @@
 import sys
 import time
+import wikipedia
 
 isRunning = True
 
+
+def askWiki(string):
+    print(wikipedia.summary(string))
+
+
 def answerMaker():
+    print("Type your words:")
     qurie = input()
 
     if qurie == "Who are you":
@@ -11,6 +18,9 @@ def answerMaker():
 
     elif qurie == "What is your porpose":
         print("I am here to help you in web search and chat a bit\n")
+    
+    elif qurie == "What can you do":
+        print("I can answer to you and sear wikipedia")
 
     elif qurie == "What is your name":
         print("My name is Eric, sir\n")
@@ -21,11 +31,19 @@ def answerMaker():
     elif qurie == "How are you":
         print("I am a bot but I wish you are fine\n")
 
+    elif qurie == "Search":
+        print("Enter search qurie:")
+        searchQurie = input()
+        askWiki(searchQurie)
+        print("\n")
+        
+
     elif qurie == "Bye":
         print("Bye\nSee You next time")
         time.sleep(1.5)
         sys.exit()
         isRunning = False
+    
     else:
         print("I can answer only limited questions\n It is a prototye version")
         print("Or type again in first word capital\n")
